4103b6a5dc39e03168682fd8295377c730418547
Gago-H <95845046+Gago-H@users.noreply.github.com>
2025-04-29 12:37:21 -0700

    Revert "Use Select Menus and Buttons to make a more interactive display for trivia. Fix the error with deferReply and followUp."
    This reverts commit 583a334b1743fd9922ee851d054b835ba6103f49.

--- commands/fun/trivia.js
+++ commands/fun/trivia.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const { StringSelectMenuBuild, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
     name: 'trivia',
@@ -116,56 +115,56 @@ module.exports = {
 function buildURL(args) {
     let baseurl = 'https://opentdb.com/api.php?';
 
-    if (parseInt(args[0]) < 5 || parseInt(args[0]) > 30) {
-        return message.reply("Please choose a number of questions from 5-30. Your selection is out of range.");
-    } else {
-        baseurl += `amount=${args[0]}`;
+        if (parseInt(args[0]) < 5 || parseInt(args[0]) > 30){
+            return message.reply("Please choose a number of questions from 5-30. Your selection is out of range.");
+        } else {
+            baseurl += `amount=${args[0]}`;
+        }
+
+        if (parseInt(args[1]) < 9 || parseInt(args[1]) > 32){
+            return message.reply("Please choose a category ID from the list provided. If you need to see the list use !triviahelp.")
+        } else if (args[1] == 'any') {
+            baseurl += '';
+        } else {
+            baseurl += `&category=${args[1]}`;
+        }
+
+        switch(args[2]){
+            case 'any':
+                break;
+            case 'easy':
+                baseurl += `&difficulty=${args[2]}`;
+                break;
+            case 'medium':
+                baseurl += `&difficulty=${args[2]}`;
+                break;
+            case 'hard':
+                baseurl += `&difficulty=${args[2]}`;
+                break;
+            default:
+                return message.reply("Please choose a difficulty: any, easy, medium, hard. ");
+        }
+
+        switch(args[3]){
+            case 'any':
+                break;
+            case 'mc':
+                baseurl += `&type=multiple`;
+                break;
+            case 'tf':
+                baseurl += `&type=boolean`;
+                break;
+            default:
+                return message.reply("Please choose a game type: any, mc, tf. (mc = multiple choice, tf = true/false)");
+        }
+
+        const response = await fetch(baseurl);
+
+        const data = await response.json();
+
+        
+        console.log(args);
+        console.log(baseurl);
+        console.log(data);
     }
-
-    if (parseInt(args[1]) < 9 || parseInt(args[1]) > 32) {
-        return message.reply("Please choose a category ID from the list provided. If you need to see the list use !triviahelp.")
-    } else if (args[1] == 'any') {
-        baseurl += '';
-    } else {
-        baseurl += `&category=${args[1]}`;
-    }
-
-    switch (args[2]) {
-        case 'any':
-            break;
-        case 'easy':
-            baseurl += `&difficulty=${args[2]}`;
-            break;
-        case 'medium':
-            baseurl += `&difficulty=${args[2]}`;
-            break;
-        case 'hard':
-            baseurl += `&difficulty=${args[2]}`;
-            break;
-        default:
-            return message.reply("Please choose a difficulty: any, easy, medium, hard. ");
-    }
-
-    switch (args[3]) {
-        case 'any':
-            break;
-        case 'mc':
-            baseurl += `&type=multiple`;
-            break;
-        case 'tf':
-            baseurl += `&type=boolean`;
-            break;
-        default:
-            return message.reply("Please choose a game type: any, mc, tf. (mc = multiple choice, tf = true/false)");
-    }
-
-    return baseurl;
-}
-
-function decodeHTMLEntities(text) {
-    return text.replace(/&quot;/g, '"')
-        .replace(/&#039;/g, "'")
-        .replace(/&amp;/g, '&')
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>');
 }
